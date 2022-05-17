aa6d4493efe9eb84af032a13a320921a1da70fb1
Folknology <al@folknology.com>
2022-05-17 16:32:48 +0100

    Added blades and UArt pins

--- mystorm_boards/icelogicbus.py
+++ mystorm_boards/icelogicbus.py
@@ -10,12 +10,18 @@ __all__ = ["IceLogicBusPlatform"]
 
 # Pinout definitions
 LED =" J11" #B7
-# UART = " J2 K2" #Rx,Tx
+RX = " K7"
+TX = " L7"
 SPI =" K9 J9 L10 L7" #si,so, sck,ss
 TILE1 = " F1 F2 F4 F3 G1 G2 G3 H3 H1 H2 J1 K1" #C
 TILE2 = " K11 H10 J10 G8 H11 G10 D9 E11 G9 G11 F10 F9" #G
 TILE3= " B1 B2 C4 C3 C2 C1 E1 D1 D2 D3 E2 E3" #B
 TILE4 = " A9 C8 A8 C7 A6 A5 A2 A3 F11 E9 E10 E8" #F
+
+BLADE1 = " K7 L7 J7 H7 L8 J8"
+BLADE2 = " B3 A1 A4 B4 B5 D5"
+BLADE3 = " D7 B6 A7 B8 B9 C9"
+BLADE4 = " D10 D11 C11 B11 A11 A10"
 # USER = " J11 B6 D7 B7"
 # SUPER = " B4 A4 A1 B3 _ _ _ _"
 GENPINS = " - - - - - - - -"
@@ -40,7 +46,12 @@ class IceLogicBusPlatform(LatticeICE40Platform):
         # led
         Resource("led", 0, Pins(LED, dir="o", invert=True),
                  Attrs(IO_STANDARD="SB_LVCMOS")
-
+                 ),
+        Resource("tx", 0, Pins(TX, dir="o"),
+                 Attrs(IO_STANDARD="SB_LVCMOS")
+                 ),
+        Resource("rx", 0, Pins(RX, dir="o"),
+                 Attrs(IO_STANDARD="SB_LVCMOS")
                  ),
         # SPI
         # Resource("sck", 0, Pins("L10", dir="o"), Attrs(IO_STANDARD="SB_LVCMOS")),
@@ -56,11 +67,11 @@ class IceLogicBusPlatform(LatticeICE40Platform):
         Resource("qss", 0, Pins("K10", dir="i"), Attrs(IO_STANDARD="SB_LVCMOS")),
         Resource("qdr", 0, Pins("J7", dir="i"), Attrs(IO_STANDARD="SB_LVCMOS")),
         # Uart
-        # UARTResource(0,
-        #              rx="J2", tx="K2",
-        #              attrs=Attrs(IO_STANDARD="SB_LVCMOS", PULLUP=1),
-        #              role="dce"
-        #              ),
+        UARTResource(0,
+                     rx="K7", tx="L7",
+                     attrs=Attrs(IO_STANDARD="SB_LVCMOS", PULLUP=1),
+                     role="dce"
+                     ),
         # *SPIFlashResources(0,
         #                    cs_n="L7", clk="L10", copi="J9", cipo="K9",
         #                    attrs=Attrs(IO_STANDARD="SB_LVCMOS")
@@ -73,6 +84,11 @@ class IceLogicBusPlatform(LatticeICE40Platform):
         Connector("tile", 2, TILE2 + GENPINS),  #
         Connector("tile", 3, TILE3 + GENPINS),  #
         Connector("tile", 4, TILE4 + GENPINS),  #
+        # Micro Blade connectores
+        Connector("blade", 1, BLADE1),
+        Connector("blade", 2, BLADE2),
+        Connector("blade", 3, BLADE3),
+        Connector("blade", 4, BLADE4),
         # Mezzanine Connectors
         Connector("mez", 0, MEZZA),  #
         Connector("mez", 1, MEZZB),  #
@@ -109,8 +125,19 @@ class IceLogicBusPlatform(LatticeICE40Platform):
     #             with open(products.extract("{}.bin".format(name))) as f:
     #                 ser.write(f.read())
 
+    def bus_send(self, bytes2send):
+        if self.upload_port is None:
+            self.get_port()
+            if self.upload_port is None:
+                print("could not find a suitable device for upload port, cannot upload")
+                return
+            else:
+                print("Found device for uploading: ", self.upload_port.product + ' as device ' + self.upload_port.device)
 
+        with serial.Serial(self.upload_port.device) as ser:
+            ser.write(bytes2send)
 
+#bus_send(bytearray(b'\x00\xff'))
 
 if __name__ == "__main__":
     #from .test.blinky import *
